2a230906fa331e3ccfe2b9103486898def12f261
hiroyuki ishida <ninomiya4.soccer.nex@gmail.com>
2021-09-09 00:37:11 +0900

    dsa

--- main.py
+++ main.py
@@ -55,6 +55,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
+    line_bot_api.push_message(developer_id,
+        TextSendMessage(text=text))
 
     if text == 'おはよう':
         line_bot_api.reply_message(
