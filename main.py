16dc8096e5622a155fbd14cf2bebba880e946cf9
hiroyuki ishida <ninomiya4.soccer.nex@gmail.com>
2021-09-09 00:03:28 +0900

    dsa

--- main.py
+++ main.py
@@ -72,10 +72,12 @@ def handle_message(event):
 model = load_model('my_model.h5') # 学習済みモデルをロードする
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_content_message(event):
+    print("----3----")
     print(event.message.id)
     print(event.message.type)
     print(event.message.text)
-    line_bot_api.push_message(developer_id,TextSendMessage(text=event.message))
+    print("----4----")
+    line_bot_api.push_message(developer_id,TextSendMessage(text=event.message.text))
     message_content = line_bot_api.get_message_content(event.message.id)
     with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix="jpg" + '-', delete=False) as tf:
         for chunk in message_content.iter_content():
