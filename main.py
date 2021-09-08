321d8f672dd3acb373a8bfa854d65db167d2beba
hiroyuki ishida <ninomiya4.soccer.nex@gmail.com>
2021-09-09 00:24:22 +0900

    dsa

--- main.py
+++ main.py
@@ -80,12 +80,13 @@ def handle_content_message(event):
     print(static_tmp_path)
     print("----4----")
     line_bot_api.push_message(developer_id,
-        TextSendMessage(text=static_tmp_path))
+        TextSendMessage(text=static_tmp_path+"/"))
     message_content = line_bot_api.get_message_content(event.message.id)
     with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix="jpg" + '-', delete=False) as tf:
         for chunk in message_content.iter_content():
             tf.write(chunk)
             tempfile_path = tf.name
+    print(tempfile_path)
 
     dist_path = tempfile_path + '.' + "jpg"
     dist_name = os.path.basename(dist_path)
