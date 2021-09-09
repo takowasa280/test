a22cdd867a8b2acbfd2cb7956f58a066114cae66
hiroyuki ishida <ninomiya4.soccer.nex@gmail.com>
2021-09-09 20:28:24 +0900

    ss

--- main.py
+++ main.py
@@ -70,6 +70,8 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='完全一致の時は1、部分一致の時は2を入力してねぇ'))
+            df=df.append({'input' : 'text' , 'type' : 'text', 'option' : 1, 'output' : "aa"} , ignore_index=True)
+            print（df）
         @handler.add(MessageEvent, message=TextMessage)
         def handle_message(event):
             text2 = event.message.text
