8c7df57c6f8cff0095f8788cdb106d6803e007ba
hiroyuki ishida <ninomiya4.soccer.nex@gmail.com>
2021-09-09 20:32:32 +0900

    ss

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='完全一致の時は1、部分一致の時は2を入力してねぇ'))
-            df=df.append({'input' : 'text' , 'type' : 'text', 'option' : 1, 'output' : "aa"} , ignore_index=True)
+            df.append({'input' : 'text' , 'type' : 'text', 'option' : 1, 'output' : "aa"} , ignore_index=True)
             df.to_csv("text.csv")
             print(df)
         @handler.add(MessageEvent, message=TextMessage)
